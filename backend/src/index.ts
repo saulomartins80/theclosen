0a1ad19e43dcbf93aa887b85053161cf4afeb2c8
saulomartins80 <saulochagas29@gmail.com>
2025-03-08 17:54:02 -0300

    Correções no backend e frontend para deploy no Vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Rota raiz
+app.get("/", (req, res) => {
+  res.send("Backend está funcionando!");
+});
+
 // Routes
 app.use("/api", transacoesRoutes);
 app.use("/api", goalsRoutes); // add goals routes
@@ -20,13 +25,13 @@ app.use("/api", investimentoRoutes);
 
 // Connect to MongoDB
 mongoose
-    .connect(process.env.MONGO_URI!)
-    .then(() => {
-        console.log("Connected to MongoDB.");
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}.`);
-        });
-    })
-    .catch((error: any) => {
-        console.error("Error connecting to MongoDB:", error);
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log("Connected to MongoDB.");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}.`);
     });
+  })
+  .catch((error: any) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
