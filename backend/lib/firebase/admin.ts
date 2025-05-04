b286350c8da819ea3e2acfc54439e95a7a91e245
saulomartins80 <saulochagas29@gmail.com>
2025-05-04 09:31:55 -0300

    chore(backend): adiciona configurações do Firebase

---
+++ backend/lib/firebase/admin.ts
@@ -0,0 +1,11 @@
+// lib/firebase/admin.ts
+import { cert, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}');
+
+const app = initializeApp({
+  credential: cert(serviceAccount)
+});
+
+export const adminFirestore = getFirestore(app);
